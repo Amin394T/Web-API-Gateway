eeb01ee585ac06e7c88368b0b1329ccf6aeb029c
Amin394 <amin394@outlook.com>
2026-02-02 23:37:08 +0100

    pulled route data from database

--- source/router.js
+++ source/router.js
@@ -1,17 +1,31 @@
 import { Router } from "express";
-import routes from "../utilities/data.js";
+import { db } from "../app.js";
 
 const router = Router();
 
-router.use((req, res, next) => {
-  for (const route of routes) {
-    if (route.path != req.path) continue;
-    if (route.method != req.method) continue;
-    
+router.use(async (req, res, next) => {
+  try {
+    const query = `
+      SELECT id, description, module_id, path, method, cache, active, code
+      FROM api_routes
+      WHERE active = true AND path = $1 AND method = $2
+      LIMIT 1
+    `;
+
+    const { rows } = await db.query(query, [req.path, req.method]);
+
+    if (!rows || rows.length == 0) return next();
+
+    const route = rows[0];
+    if (!route.code) return next();
+
     const response = new Function('req', route.code)(req);
     return res.status(response.status).json(response.data);
   }
-  next();
+  catch(err) {
+    next(err);
+  }
 });
+
 
 export default router;
